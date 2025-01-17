3d5e856ecb000f7603fd71a22f172fc39038ded3
Tommy Johnsen <7oiden@gmail.com>
2025-01-17 12:38:11 +0100

    reroute when no id

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -56,39 +56,43 @@ const Create = () => {
     }
   };
 
+  if (!userId) {
+    navigate("/");
+  }
+
   return (
     <div className="page create">
       {userId ? (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
 
-        <label htmlFor="method">Method:</label>
-        <textarea
-          id="method"
-          value={method}
-          onChange={(e) => setMethod(e.target.value)}
-        />
+          <label htmlFor="method">Method:</label>
+          <textarea
+            id="method"
+            value={method}
+            onChange={(e) => setMethod(e.target.value)}
+          />
 
-        <label htmlFor="rating">Rating:</label>
-        <input
-          type="number"
-          id="rating"
-          value={rating}
-          onChange={(e) => setRating(e.target.value)}
-        />
+          <label htmlFor="rating">Rating:</label>
+          <input
+            type="number"
+            id="rating"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+          />
 
-        <button>Create Smoothie Recipe</button>
+          <button>Create Smoothie Recipe</button>
 
-        {formError && <p className="error">{formError}</p>}
-      </form>
+          {formError && <p className="error">{formError}</p>}
+        </form>
       ) : (
-      <p>Please log in to create a smoothie.</p>
+        <p>Please log in to create a smoothie.</p>
       )}
     </div>
   );
